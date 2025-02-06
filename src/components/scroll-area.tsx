f890da1f113aa3057d190b44b3491bc7cded870b
Danylo Ohurtsov <d.ohurtsov.fed@gmail.com>
2025-02-06 12:52:37 +0100

    feat: enhance ScrollArea component with scrollbar toggle
    - Added the ability to disable scrollbar visibility in `scroll-area.tsx`.

--- src/components/scroll-area.tsx
+++ src/components/scroll-area.tsx
@@ -1,12 +1,23 @@
+import { cn } from "@/lib/utils";
 import React from "react";
 
 interface ScrollAreaProps {
   children: React.ReactNode;
+  isScrollbarVisible?: boolean;
 }
 
-const ScrollArea = ({ children }: ScrollAreaProps) => {
+const ScrollArea = ({
+  children,
+  isScrollbarVisible = true,
+}: ScrollAreaProps) => {
   return (
-    <div className="h-full overflow-y-auto pl-1 pr-2 [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-300 dark:[&::-webkit-scrollbar-track]:bg-neutral-700 dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500">
+    <div
+      className={cn(
+        "h-full overflow-y-auto pl-1 pr-2 [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-300 dark:[&::-webkit-scrollbar-track]:bg-neutral-700 dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500",
+
+        isScrollbarVisible === false && "[&::-webkit-scrollbar]:w-0"
+      )}
+    >
       {children}
     </div>
   );
