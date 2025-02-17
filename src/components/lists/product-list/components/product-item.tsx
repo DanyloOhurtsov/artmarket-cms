2b24375c4c5dc5fb4fb4c645b8df570d501ca79f
Danylo Ohurtsov <d.ohurtsov.fed@gmail.com>
2025-02-17 20:04:10 +0100

    feat: add product item component for product list table
    - Created a reusable component to represent a single product in the product list table.

--- src/components/lists/product-list/components/product-item.tsx
+++ src/components/lists/product-list/components/product-item.tsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import { cn } from "@/lib/utils";
+import { flexRender, Row } from "@tanstack/react-table";
+import * as TableComponent from "@/components/ui/table";
+import { ProductType } from "@/lib/schemas/new/product.schema";
 
-const ProductItem = () => {
+const ProductItem = ({ row }: { row: Row<ProductType> }) => {
+  const url = `/dashboard/products/${row.original.handle}`;
+
   return (
-    <div>ProductItem</div>
-  )
-}
+    <TableComponent.TableRow
+      data-state={row.getIsSelected() && "selected"}
+      className="cursor-pointer"
+      onClick={() => (window.location.href = url)}
+    >
+      {row.getVisibleCells().map((cell) => (
+        <TableComponent.TableCell
+          key={cell.id}
+          className={cn(
+            cell.column.id === "select" && "w-6",
+            cell.column.id === "separator" && "w-6",
+            cell.column.id === "title" && "pl-0"
+          )}
+        >
+          {cell.column.id === "select" ? (
+            <div
+              onClick={(e) => e.stopPropagation()}
+              className="h-full flex items-center"
+            >
+              {flexRender(cell.column.columnDef.cell, cell.getContext())}
+            </div>
+          ) : (
+            flexRender(cell.column.columnDef.cell, cell.getContext())
+          )}
+        </TableComponent.TableCell>
+      ))}
+    </TableComponent.TableRow>
+  );
+};
 
-export default ProductItem
+export default ProductItem;
