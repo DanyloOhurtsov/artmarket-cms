6ea5fad780e692f2a35d9b5d00a4de0b310b78f1
Danylo Ohurtsov <d.ohurtsov.fed@gmail.com>
2025-02-22 13:00:03 +0100

    feat: add skeleton loader for product list
    - Implemented a loading skeleton for the product list to enhance user experience.

--- src/app/(protected)/dashboard/collections/[handle]/_components/product-list/product-list-collection.skeleton.tsx
+++ src/app/(protected)/dashboard/collections/[handle]/_components/product-list/product-list-collection.skeleton.tsx
@@ -1,3 +1,6 @@
+import { Skeleton } from "@/components/ui/skeleton";
+import * as TableComponent from "@/components/ui/table";
+
 const ProductListCollectionSkeleton = ({
   isEmpty = false,
 }: {
@@ -9,7 +12,71 @@ const ProductListCollectionSkeleton = ({
         <p>пусто</p>
       ) : (
         <>
-          <div>Завантаження</div>
+          <div className="flex flex-col gap-y-4 p-4">
+            <div className="flex justify-between items-center">
+              <Skeleton className="w-52 h-10" />
+
+              <Skeleton className="w-12 h-10" />
+            </div>
+
+            <TableComponent.Table>
+              <TableComponent.TableHeader>
+                <TableComponent.TableRow className="hover:bg-transparent">
+                  <TableComponent.TableHead className="pr-0">
+                    <Skeleton className="w-6 h-6" />
+                  </TableComponent.TableHead>
+
+                  <TableComponent.TableHead className="w-2">
+                    <div className="w-px h-6 bg-primary/20 mx-auto" />
+                  </TableComponent.TableHead>
+
+                  <TableComponent.TableHead className="pl-0">
+                    <div className="pl-10">
+                      <Skeleton className="w-24 h-6" />
+                    </div>
+                  </TableComponent.TableHead>
+
+                  <TableComponent.TableHead>
+                    <Skeleton className="w-24 h-6" />
+                  </TableComponent.TableHead>
+
+                  <TableComponent.TableHead>
+                    <Skeleton className="w-24 h-6" />
+                  </TableComponent.TableHead>
+                </TableComponent.TableRow>
+              </TableComponent.TableHeader>
+
+              <TableComponent.TableBody>
+                {Array(5)
+                  .fill("")
+                  .map((_, idx) => (
+                    <TableComponent.TableRow key={idx}>
+                      <TableComponent.TableHead className="pr-0">
+                        <Skeleton className="w-6 h-6" />
+                      </TableComponent.TableHead>
+
+                      <TableComponent.TableHead className="w-2">
+                        <div className="w-px h-6 bg-primary/20 mx-auto" />
+                      </TableComponent.TableHead>
+
+                      <TableComponent.TableHead className="pl-0">
+                        <div className="pl-10">
+                          <Skeleton className="w-24 h-6" />
+                        </div>
+                      </TableComponent.TableHead>
+
+                      <TableComponent.TableHead>
+                        <Skeleton className="w-24 h-6" />
+                      </TableComponent.TableHead>
+
+                      <TableComponent.TableHead>
+                        <Skeleton className="w-24 h-6" />
+                      </TableComponent.TableHead>
+                    </TableComponent.TableRow>
+                  ))}
+              </TableComponent.TableBody>
+            </TableComponent.Table>
+          </div>
         </>
       )}
     </>
